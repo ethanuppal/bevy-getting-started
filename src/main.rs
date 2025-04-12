791119f8360af831881b50bd0f5568e542877bd4
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2025-04-12 01:02:07 -0400

    feat: First query
    Signed-off-by: Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(&'static str);
+
 fn hello_world() {
     println!("Hello world");
 }
 
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Sans Undertale")));
+    commands.spawn((Person, Name("Lorenzo Alvisi")));
+    commands.spawn((Person, Name("PPAP")));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("Hello {}", name.0);
+    }
+}
+
 fn main() {
-    App::new().add_systems(Update, hello_world).run();
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, greet_people))
+        .run();
 }
