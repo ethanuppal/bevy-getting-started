4bed7f7b86b31f42128d33d98053bfae1c7ef618
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2025-04-12 01:29:56 -0400

    fix: Timer delay
    Signed-off-by: Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.9, TimerMode::Repeating)));
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
         app.add_systems(Startup, add_people);
         app.add_systems(Update, (update_people, greet_people).chain());
     }
