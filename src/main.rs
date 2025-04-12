879e6390974d218766cb5baf3302497ed13ddf21
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2025-04-12 01:10:26 -0400

    feat: Timer resource
    Signed-off-by: Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,8 @@ struct Person;
 #[derive(Component)]
 struct Name(&'static str);
 
-fn hello_world() {
-    println!("Hello world");
-}
+#[derive(Resource)]
+struct GreetTimer(Timer);
 
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Sans Undertale")));
@@ -16,9 +15,11 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("PPAP")));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("Hello {}", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("Hello {}", name.0);
+        }
     }
 }
 
@@ -34,8 +35,9 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.9, TimerMode::Repeating)));
         app.add_systems(Startup, add_people);
-        app.add_systems(Update, (hello_world, (update_people, greet_people).chain()));
+        app.add_systems(Update, (update_people, greet_people).chain());
     }
 }
 
